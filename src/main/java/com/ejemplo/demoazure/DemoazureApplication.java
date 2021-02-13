4d306b65ea6382c09026eab0f1b975704ec18206
ing-berrocal <ingeniero.berrocal@hotmail.com>
2021-02-12 22:22:10 -0500

    primer commit azure.activedirectory.tenant-id=aa31ad36-a81e-44b4-b370-be39b3e60540 azure.activedirectory.client-id=b9c874ce-ca5d-421a-9110-aa78282e88fd azure.activedirectory.client-secret=Ghh1.HPcpF_DV6aR9PbfI39\~Gr0\~o5o9MV azure.activedirectory.user-group.allowed-groups=grupo_prueba logging.level.web=DEBUG

---
+++ src/main/java/com/ejemplo/demoazure/DemoazureApplication.java
@@ -0,0 +1,13 @@
+package com.ejemplo.demoazure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoazureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoazureApplication.class, args);
+	}
+
+}
